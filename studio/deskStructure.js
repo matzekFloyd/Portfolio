9cb45c1e52f8f3408de0987804301ead2fb03523
Mathias Mayrhofer <it181508@fhstp.ac.at>
2020-09-11 14:11:09 +0200

    add singleton "about" structure

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -2,7 +2,7 @@ import S from '@sanity/desk-tool/structure-builder'
 import MdSettings from 'react-icons/lib/md/settings'
 
 const hiddenDocTypes = listItem =>
-  !['category', 'person', 'sampleProject', 'siteSettings'].includes(listItem.getId())
+  !['about', 'category', 'person', 'sampleProject', 'siteSettings'].includes(listItem.getId())
 
 export default () =>
   S.list()
@@ -29,6 +29,14 @@ export default () =>
         .title('Categories')
         .schemaType('category')
         .child(S.documentTypeList('category').title('Categories')),
+      S.listItem()
+        .title('About Me')
+        .child(
+          S.editor()
+            .id('about')
+            .schemaType('about')
+            .documentId('singleton-about')
+        ),
       // This returns an array of all the document types
       // defined in schema.js. We filter out those that we have
       // defined the structure above
