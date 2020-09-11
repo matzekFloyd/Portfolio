f37408930f7a674cca64ad14af32a45aa4560a80
Mathias Mayrhofer <it181508@fhstp.ac.at>
2020-09-11 14:58:01 +0200

    change font family and add placeholder links to navigation

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -19,7 +19,13 @@ const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
       <nav className={cn(styles.nav, showNav && styles.showNav)}>
         <ul>
           <li>
-            <Link to='/archive/'>Archive</Link>
+            <Link to='/about/'>About Me</Link>
+          </li>
+          <li>
+            <Link to='/archive/'>Projects</Link>
+          </li>
+          <li>
+            <Link to='/contact/'>Contact</Link>
           </li>
         </ul>
       </nav>
