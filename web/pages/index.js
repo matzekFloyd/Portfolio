2a0241848a42203d70a3dcf5b49251337da1017f
Mathias Mayrhofer <mathiaspmayrhofer@gmail.com>
2026-05-26 15:30:39 +0200

    Update index.js

--- web/pages/index.js
+++ web/pages/index.js
@@ -31,7 +31,7 @@ export default function HomePage({site, projects}) {
         ) : null}
         <div className={pageStyles.description}>
           <p className={pageStyles.kicker}>Software Engineer</p>
-          <h2>{site?.subtitle || 'Software Developer from Vienna, Austria.'}</h2>
+          <h2>{site?.subtitle || 'Software Engineer from Vienna, Austria.'}</h2>
           <p>{site?.description}</p>
           <p className={pageStyles.ctaWrap}>
             <span className={pageStyles.ctaLinks}>
